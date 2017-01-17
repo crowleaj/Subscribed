78816d9c6e6ff4d451c1bbd19e8faa5fe3a40a44
Aaron Bartee <barteeaj@rose-hulman.edu>
2017-01-16 22:31:03 -0500

    Fixed nav_manage error

--- app/src/main/java/edu/rose_hulman/crowleaj/subscribed/MainActivity.java
+++ app/src/main/java/edu/rose_hulman/crowleaj/subscribed/MainActivity.java
@@ -129,8 +129,6 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
