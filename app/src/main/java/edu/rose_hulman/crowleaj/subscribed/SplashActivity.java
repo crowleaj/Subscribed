b0f445cc3e17dafa71ac62cc4074461f17d09273
Aaron Bartee <barteeaj@rose-hulman.edu>
2017-01-16 22:14:21 -0500

    Revert "Gmail API code"
    This reverts commit 3f8616706902d38f08be6494b886330686b641fe.

--- app/src/main/java/edu/rose_hulman/crowleaj/subscribed/SplashActivity.java
+++ app/src/main/java/edu/rose_hulman/crowleaj/subscribed/SplashActivity.java
@@ -3,22 +3,8 @@ package edu.rose_hulman.crowleaj.subscribed;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.services.gmail.GmailScopes;
-
 public class SplashActivity extends AppCompatActivity {
 
-    static final int REQUEST_ACCOUNT_PICKER = 1000;
-    static final int REQUEST_AUTHORIZATION = 1001;
-    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
-    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
-
-    private static final String PREF_ACCOUNT_NAME = "accountName";
-
-    private static final String[] SCOPES = { GmailScopes.GMAIL_LABELS };
-
-
-    GoogleAccountCredential mCredential;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
