fc81baf9defc952b7a47c060e5974691fbf5a9d4
Aaron Bartee <barteeaj@rose-hulman.edu>
2017-01-22 16:02:56 -0500

    Added login layout

--- app/src/main/java/edu/rose_hulman/crowleaj/subscribed/MainActivity.java
+++ app/src/main/java/edu/rose_hulman/crowleaj/subscribed/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
